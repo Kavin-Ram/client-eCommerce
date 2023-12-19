a38334b53a86aedc55f1552eaf04ec4032997b57
Kavin-Ram <150883023+Kavin-Ram@users.noreply.github.com>
2023-12-19 17:46:30 +0530

    url change

--- src/Config/url_configuration.js
+++ src/Config/url_configuration.js
@@ -1,6 +1,7 @@
 // config.js
 const url_configuration = {
-    BASE_URL: 'http://localhost:5000'
+    // BASE_URL: 'http://localhost:5000'
+    BASE_URL: 'https://odd-ruby-hippo-wear.cyclic.app/'
 };
 
 export default url_configuration;
